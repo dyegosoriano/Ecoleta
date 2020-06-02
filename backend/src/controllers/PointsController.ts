158a77aef9b4682a85c98d18cfe2bb63efff5c49
Dyego Soriano <dyegodf2@gmail.com>
2020-06-02 17:32:48 -0300

    add route show

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -17,6 +17,20 @@ class PointsController {
 
     return response.json({ id: point_id, ...point })
   }
+
+  async show (request: Request, response: Response) {
+    const { id } = request.params
+    const point = await knex('points').where('id', id).first()
+
+    if (!point) { return response.status(400).json({ message: 'Point not found.' }) }
+
+    const items = await knex('items')
+      .join('point_items', 'items.id', '=', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.title')
+
+    return response.json({ point, items })
+  }
 }
 
 export default new PointsController()
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -8,5 +8,6 @@ const routes = express.Router()
 routes
   .get('/items', ItemsController.index)
   .post('/points', PointsController.store)
+  .get('/points/:id', PointsController.show)
 
 export default routes
