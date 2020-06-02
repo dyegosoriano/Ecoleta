c67cde013a1de2d0c0d1b3fa26a01d1a76dd7b5e
Dyego Soriano <dyegodf2@gmail.com>
2020-06-02 16:51:52 -0300

    signing up collection point

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 
 import ItemController from './controllers/ItemController'
+import PointsController from './controllers/PointsController'
 
 const routes = express.Router()
 
-routes.get('/items', ItemController.index)
+routes
+  .get('/items', ItemController.index)
+  .post('/points', PointsController.store)
 
 export default routes
