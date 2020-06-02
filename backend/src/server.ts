d310defd4dcdfb229b297059a92bc12bf870af6e
Dyego Soriano <dyegodf2@gmail.com>
2020-06-02 15:24:27 -0300

    accessing files in uploads folders

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import routes from './routes'
+import { resolve } from 'path'
 
 const app = express()
 
 app.use(express.json())
-app.use(routes)
+  .use(routes)
+  .use('/uploads', express.static(resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333)
